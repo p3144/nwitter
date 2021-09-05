d3f6259f8e3f64d7aee3316d0ef12a92f4a8bf50
jypark <jjun.park@gmail.com>
2021-09-05 15:47:29 +0900

    finished basic config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvL3lqCiK5mUBeMendcb4wfmlOOKVXZtM",
+  authDomain: "nwitter-6efa7.firebaseapp.com",
+  projectId: "nwitter-6efa7",
+  storageBucket: "nwitter-6efa7.appspot.com",
+  messagingSenderId: "127147370537",
+  appId: "1:127147370537:web:c446b0d47c0ecf9bd09438"
+};
+
+export default firebase.initializeApp(firebaseConfig);
+
+
